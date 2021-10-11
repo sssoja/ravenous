7f72c18574c3fe487f2e5be3dac0d2e428326bdf
Sophie <sophie@sssoja.com>
2021-10-11 19:57:18 +0100

    bugfix fix pathname and missing state prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,22 +3,24 @@ import "./App.css";
 
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-import { Yelp } from "../../util/Yelp";
+
+import Yelp from "../../util/Yelp";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       businesses: [],
     };
+
     this.searchYelp = this.searchYelp.bind(this);
   }
+
   searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(businesses =>
-      this.setState({
-        businesses: businesses,
-      })
-    );
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({ businesses: businesses });
+    });
   }
 
   render() {
@@ -26,7 +28,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
